337fdafd35efa9485b97892079bcd494ab2dbe66
Forrest Born <97557895+Mori2190@users.noreply.github.com>
2022-11-23 13:20:14 -0800

    Add files via upload

---
+++ Python/Tutorial-Files/variable1.py
@@ -0,0 +1,12 @@
+# This tutorial is available at: "https://www.tutorialspoint.com/python/index.htm"
+
+# This is my first Python variable program.
+#!/usr/bin/python
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print ("The counter is at: "counter)
+print ("The miles are at: "miles)
+print ("Your name is: "name)
