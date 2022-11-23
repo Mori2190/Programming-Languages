cc9bff250a2208f8203afa26ba6f64a0ac2d4867
Forrest Born <97557895+Mori2190@users.noreply.github.com>
2022-11-23 13:08:37 -0800

    Add files via upload

---
+++ Python/Tutorial-Files/hello-world.py
@@ -0,0 +1,6 @@
+# This tutorial is available at: "https://www.tutorialspoint.com/python/index.htm"
+
+# This is my first Python program.
+# This will print 'Hello, World!' as the output
+
+print ("Hello, World!");
