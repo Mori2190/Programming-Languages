c5bd1d6aab4137a053e559daa9e52f3a573c0376
Forrest Born <97557895+Mori2190@users.noreply.github.com>
2022-11-23 15:30:37 -0500

    Delete Modern-Fortran directory

--- Modern-Fortran/Tutorial-Files/hello.f90
+++
@@ -1,6 +0,0 @@
-! This is a Fortran Tutorial file located at: https://fortran-lang.org/en/learn/quickstart/
-program hello
-   ! This is a comment line; it is ignored by the compiler
-   print *, 'Hello, World!'
-end program hello
-   ! Save this file as hello.f90 and run it in the compiler by; gfortran hello.f90 -o hello
