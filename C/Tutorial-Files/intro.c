cca3be367b08a8c04498d3a57f8e3385eed427b9
Forrest Born <fborn2190@gmail.com>
2023-04-26 19:22:37 -0400

    Update intro.c

--- C/Tutorial-Files/intro.c
+++ C/Tutorial-Files/intro.c
@@ -1,9 +1,14 @@
-// Tutorial located at: https://www.cprogramming.com/tutorial/c/lesson1.html
-// my first program in C
-#include <stdio.h>
-int main()
+//  my first program in C
+//  This is a Line comment.
+/*  This is a block comment.
+    All comments are read by compilers as a single space character.
+    Block comments begin with the slash-astrix
+    and ends with the astrix-slash.*/
+#include <stdio.h> //This is to include the stdio.h file in this program.
+
+int main() //The main function is required in C and CPP.
 {
-	printf( "I am alive! Beware.\n" );
-	getchar();
-	return 0;
+    printf( "I am alive! Beware.\n" );
+    getchar();
+    return 0; //Returns an error code and ends the function. Useful for debuging faulty program code.
 }
